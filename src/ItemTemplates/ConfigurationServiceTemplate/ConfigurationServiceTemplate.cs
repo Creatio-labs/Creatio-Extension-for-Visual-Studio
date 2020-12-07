4fd51e98d8b6c1a012942ae9093c740a2adfd011
Mykyta Lytvynenko <mykyta.lytvynenko@gmail.com>
2020-12-07 22:00:13 +0200

    #2 Create item template for "configuration service" #3 Create item template for "entity event listener"

---
+++ src/ItemTemplates/ConfigurationServiceTemplate/ConfigurationServiceTemplate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using Terrasoft.Web.Common;
+
+namespace $rootnamespace$
+{
+    #region Class: $safeitemrootname$
+
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class $safeitemrootname$: BaseService
+    {
+        #region Methods: Public
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped,
+                    ResponseFormat = WebMessageFormat.Json)]
+        public $safeitemrootname$Response GlbMethod1($safeitemrootname$Request request)
+        {
+            var response = new $safeitemrootname$Response();
+
+            //TODO your business logic
+
+            return response;
+        }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region Class: $safeitemrootname$Request
+    
+    [DataContract]
+    public class $safeitemrootname$Request
+    {
+        [DataMember]
+        public Guid RecordId { get; set; }
+    }
+
+    #endregion
+
+    #region Class: $safeitemrootname$Response
+
+    [DataContract]
+    public class $safeitemrootname$Response
+    {
+        [DataMember]
+        public bool IsSuccess { get; set; }
+    }
+
+    #endregion
+}
---
+++ src/ItemTemplates/EntityEventListenerTemplate/EntityEventListenerTemplate.cs
@@ -0,0 +1,97 @@
+﻿using Terrasoft.Core.Entities;
+using Terrasoft.Core.Entities.Events;
+
+namespace $rootnamespace$
+{
+    #region Class: $safeitemrootname$
+
+    [EntityEventListener(SchemaName = "SPECIFY_ENTITY_SCHEMA_NAME")]
+    public class $safeitemrootname$ : BaseEntityEventListener
+    {
+        #region Methods: Public
+
+        public override void OnInserted(object sender, EntityAfterEventArgs e)
+        {
+            base.OnInserted(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        public override void OnInserting(object sender, EntityBeforeEventArgs e)
+        {
+            base.OnInserting(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        public override void OnSaved(object sender, EntityAfterEventArgs e)
+        {
+            base.OnSaved(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        public override void OnSaving(object sender, EntityBeforeEventArgs e)
+        {
+            base.OnSaving(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        public override void OnUpdated(object sender, EntityAfterEventArgs e)
+        {
+            base.OnUpdated(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        public override void OnUpdating(object sender, EntityBeforeEventArgs e)
+        {
+            base.OnUpdating(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        public override void OnDeleted(object sender, EntityAfterEventArgs e)
+        {
+            base.OnDeleted(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        public override void OnDeleting(object sender, EntityBeforeEventArgs e)
+        {
+            base.OnDeleting(sender, e);
+
+            var entity = (Entity)sender;
+            var userConnection = entity.UserConnection;
+
+            //TODO your business logic
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
