ea7cf42ab3e61dd7ea2553094a56285b89d62af9
Mykyta Lytvynenko <mykyta.lytvynenko@gmail.com>
2020-12-08 23:56:13 +0200

    #4 Create item template for "executor" #5 Create snippet for "run business process" #6 Create snippet for "run stored procedure" #7 Create snippet for "run user defined function" #8 Create snippet for "SysSettings.GetValue" #9 Create snippet for "ClassFactory.Get"

---
+++ src/ExecutorTemplate/ExecutorTemplate.cs
@@ -0,0 +1,19 @@
+﻿using Terrasoft.Core;
+
+namespace $rootnamespace$
+{
+    #region Class: $safeitemrootname$
+
+    public class $safeitemrootname$ : IExecutor
+    {
+        #region Methods: Public 
+
+        public void Execute(UserConnection userConnection) {
+            //TOOO your code
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
